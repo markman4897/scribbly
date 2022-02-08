43d462fc9e0f38a57af72fbac544ed9f62e3e671
markman4897 <lisicic.mark@gmail.com>
2022-02-08 04:10:26 +0100

    fixed css, upgraded footer js

--- random_footer_quote.js
+++ random_footer_quote.js
@@ -12,6 +12,15 @@ let quotes = [
 let quote_interval = window.setInterval(change_quote, 30000)
 
 function change_quote() {
-  let randQuote = quotes[Math.floor(Math.random() * quotes.length)]
-  container.innerText = randQuote
+  container.classList.add('transparent')
 }
+
+
+
+container.addEventListener("transitionend", (e) => {
+  if (container.classList.contains('transparent')) {
+    let randQuote = quotes[Math.floor(Math.random() * quotes.length)]
+    container.innerText = randQuote
+    container.classList.remove('transparent')
+  }
+})
